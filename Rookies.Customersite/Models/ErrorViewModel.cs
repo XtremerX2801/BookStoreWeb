766acfc6c64d12c416888cd4b45af808b20877d6
XtremerX2801 <vdho12345678@gmail.com>
2022-02-18 15:07:44 +0700

    Change structure to fit projects

---
+++ Rookies.Customersite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Rookies.Customersite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
