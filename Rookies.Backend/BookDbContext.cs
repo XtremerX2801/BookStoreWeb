ca60e7dfd8794a8923ffa116cef9bbdbd0c3f555
XtremerX2801 <vdho12345678@gmail.com>
2022-02-21 09:54:05 +0700

    Update Layout

--- Rookies.Backend/BookDbContext.cs
+++ Rookies.Backend/BookDbContext.cs
@@ -5,9 +5,12 @@ namespace Rookies.Backend
     public class BookDbContext : DbContext
     {
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public BookDbContext()
+        {
+
+        }
+
         public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
 
         }
