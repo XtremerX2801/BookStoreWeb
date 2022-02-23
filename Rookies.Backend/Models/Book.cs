a26eac824d504e10829200be1759e23447f885a4
XtremerX2801 <vdho12345678@gmail.com>
2022-02-23 17:31:56 +0700

    Change attribute

--- Rookies.Backend/Models/Book.cs
+++ Rookies.Backend/Models/Book.cs
@@ -10,5 +10,9 @@ namespace Rookies.Backend
         [Required]
         public string BookName { get; set; }
 
-        public string? Author { get; set; }    }
+        public string? Author { get; set; }    
+
+        [Required]
+        public string BookCategory { get; set; }
+    }
 }
